3db7d3226ea5466f64fb01d13e5afc82cdb2a249
roy_benjamin <benji01560@gmail.com>
2016-08-26 02:29:08 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 var express = require('express');
+var mongodb = require('mongodb');
 var app = express();
+var path = require("path");
 var port = 8080;
 
-app.get('/', function(req, res){
+//app.use(express.static(__dirname));
+//
+//app.get("/", function(req, res) {
+//    res.sendFile(path.join(__dirname + '/index.html')); // Render HTML File
+//});
 
+var MongoClient = mongodb.MongoClient;
+var url = process.env.MONGOLAB_URI || 'mongodb://test:test@ds017246.mlab.com:17246/freecodecampdb';
 
-    res.json({
-
-    }); 
-});
-
-
-app.listen(process.env.PORT || port, function(){
-    console.log("App listening on port: " + port);
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+    throw err;
+  } 
+  else{
+    console.log('Connection established to', url);
+    db.close();
+  }
+  
+  app.listen(port, function() {
+    console.log('Listening on port: ' + port);
+  });
+  
 });
